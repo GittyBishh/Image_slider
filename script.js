0c24797bdd7b0c615258203d6c203f0441e146d6
GittyBishh <jbishnoiofficial@gmail.com>
2025-08-02 21:27:57 +0530

    better version

--- script.js
+++ script.js
@@ -2,12 +2,17 @@ const prevButton = document.querySelector(".prev");
 const nextButton = document.querySelector(".next");
 
 const slider = document.querySelector(".slider");
+const box = document.querySelector(".box");
+const page = document.querySelector(".page");
+
+
 
 let currentPosition = 0;
 const cards = 10;
 const cardwidth = 240;
+const visibleCards = 3;
 
-let maxLength = cardwidth*cards - 4*cardwidth;
+let maxLength = cardwidth*(cards - visibleCards);
 
 nextButton.addEventListener('click', ()=>{
     // if(currentPosition<()){
@@ -25,4 +30,9 @@ prevButton.addEventListener('click',()=>{
         slider.style.transform = `translate(-${currentPosition}px)`;
     }
 });
+
+box.addEventListener('click',()=>{
+    page.style.transform = `translateX(200px) rotateY(180deg)`;
+    
+})
 
